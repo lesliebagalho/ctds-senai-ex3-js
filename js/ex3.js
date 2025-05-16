fde6190eab489deeffaa3324380b5bd1e4ee909d
Leslie Bagalho <150636772+lesliebagalho@users.noreply.github.com>
2025-05-16 20:49:29 -0300

    Add files via upload

---
+++ js/ex3.js
@@ -0,0 +1,15 @@
+/*
+Instruções:
+Crie um HTML básico e inclua o link externo para o arquivo script.js
+
+No JavaScript:
+Crie uma variável com um número inteiro e use o operador módulo ( % ) para verificar se ele é par ou ímpar.
+
+Mostre no console.log() uma mensagem assim:
+console.log("O número é par?", numPar);
+
+Use:
+let
+console.log()
+*/
+
